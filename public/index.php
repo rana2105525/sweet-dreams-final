90aab71bd96f06b5a50dabbb71fce6efce1b9c36
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2023-12-01 18:19:01 +0200

    home page

--- public/index.php
+++ public/index.php
@@ -1,1 +1,28 @@
-helllooooo;
+<?php
+
+define('__ROOT__', "../app/");
+require_once(__ROOT__ . "model/User.php");
+require_once(__ROOT__ . "controller/UsersController.php");
+require_once(__ROOT__ . "view/ViewUser.php");
+
+$model = new User($_SESSION["id"]);
+$controller = new UsersController($model);
+$view = new ViewUser($controller, $model);
+
+
+// echo $view->output();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <title>Home | sweet dreams</title>
+	</head>
+<body>
+   <?php echo $view->nav();?>
+   <?php echo $view->side();?>
+   <?php echo $view->home();?>
+   <?php echo $view->footer();?>
+</body>
+</html>
