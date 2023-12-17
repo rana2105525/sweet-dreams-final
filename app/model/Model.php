ca274e5be05b70344d8b10ddc142c01b036efbb0
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2023-12-17 20:17:39 +0200

    ,

--- app/model/Model.php
+++ app/model/Model.php
@@ -5,15 +5,6 @@ require_once("../app/db/Dbh.php");
 abstract class Model{
     protected $db;
     protected $conn;
-    //protected $user_id;
-
-    
-    // public function __construct() {
-    //     $this->db = new Dbh();
-
-    //     // Retrieve user ID from session
-    //     $this->user_id = $_SESSION['id'];
-    // }
     public function connect(){
         if(null === $this->conn ){
             $this->db = new Dbh();
