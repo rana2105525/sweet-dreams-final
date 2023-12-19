9a730d463b3942629daf208e2c5db6da4347b319
aboelala16 <alamokhtar42@gmail.com>
2023-12-19 22:55:56 +0200

    user review

--- app/model/Reviews.php
+++ app/model/Reviews.php
@@ -2,45 +2,51 @@
 require_once(__ROOT__ . "model/Model.php");
 require_once(__ROOT__ . "model/Review.php");
  
-class Reviews extends Model{ 
-	private $reviews; 
-	function __construct() {
-		$this->fillArray(); 
-	}
-  
-	function fillArray() {
-		$this->reviews = array();
-		$this->db = $this->connect();
-		$result = $this->readReviews();
-		while ($row = $result->fetch_assoc()) {
-			array_push($this->reviews, new Review($row['review_id']));
-		}
-	} 
 
-	function getReviews() {
-		$this->fillArray();  
-		return $this->reviews;
-	}
+class Reviews extends Model {
+    private $reviews;
 
-	function getReview($id) {
-		foreach($this->reviews as $review) {
-			if ($id == $review->getID()) {
-				return $review;
-			}
-		}
-	}
+    function __construct() {
+        $this->fillArray();
+    }
 
-	function readReviews(){
-		$sql = "SELECT review_id, review,user_id,name,prod_id,title,p.prod_image
-		 FROM reviews r, reg u, products p 
-		 where u.id= r.user_id and p.id=r.prod_id;";
+    function fillArray() {
+        $this->reviews = array();
+        $this->db = $this->connect();
+        $result = $this->readReviews();
 
-		$result = $this->db->query($sql);
-		if ($result->num_rows > 0){
-			return $result;
-		}
-		else {
-			return false;
-		}	
-	}
+        if ($result !== false && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                array_push($this->reviews, new Review($row['review_id']));
+            }
+        } else {
+            // Handle the case where the query fails
+            // You can log an error, display a message, or take appropriate action
+            // For now, let's assume you want to display a message
+            echo "";
+        }
+    }
+
+    function getReviews() {
+        $this->fillArray();
+        return $this->reviews;
+    }
+
+    function getReview($id) {
+        foreach ($this->reviews as $review) {
+            if ($id == $review->getID()) {
+                return $review;
+            }
+        }
+    }
+
+    function readReviews() {
+        $sql = "SELECT review_id, review, user_id, name, prod_id, title, p.prod_image
+                FROM reviews r, reg u, products p 
+                WHERE u.id = r.user_id AND p.id = r.prod_id;";
+
+        $result = $this->db->query($sql);
+        return $result;
+    }
 }
+?>
